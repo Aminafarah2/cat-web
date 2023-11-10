580366b79a03654436c5048a8c3fc3c2b2aa020e
aminafarah <amishkamohamed563@gmail.com>
2023-11-10 14:36:07 +0300

    not done yet

--- cat/src/pages/Favorites.jsx
+++ cat/src/pages/Favorites.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from "axios";
+import { catOptions } from '../option';
 
  function Favorites() {
-  return (
-    <div>Favorites</div>
-  )
+    const [favorites, setFavorites] = useState(null);
+    const [catData, setCatData] = useState([]);
+    const fetchData = () => {
+      axios
+        .get(
+          `https://api.thecatapi.com/v1/favourites?sub_id=my-user-1`,
+          catOptions
+        )
+        .then((response) => setFavorites(response.data));
+    };
+  
+    useEffect(() => {
+      fetchData();
+      populateArray();
+    }, []);
+  
+    const populateArray = () => {
+      favorites?.map((favorite) => {
+        axios
+          .get(
+           `https://api.thecatapi.com/v1/images/${favorite.image_id}`,
+            catOptions
+          )
+          .then((response) =>
+            setCatData((fulldata) => [...fulldata, response.data])
+          );
+      });
+    };
+  
+    return (
+      <section className="favorite-container">
+        <h1 className="favorite-image-title">Favorites</h1>
+        <button className="favorites-button" onClick={populateArray}>
+          Load favorites
+        </button>
+        <div className="favorite-image-grid">
+          {catData?.slice(0, 6).map((cat) => (
+            <div className="image-button-pair">
+              <div> {cat.image_id}</div>
+              <img className="grid-image" src={cat.url}  alt=""/>
+            </div>
+          ))}
+        </div>
+      </section>
+    );
 }
 export default Favorites;
