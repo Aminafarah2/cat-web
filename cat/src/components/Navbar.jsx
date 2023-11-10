3cac23ff17380adf0268d637aac946dc41c0e873
aminafarah <amishkamohamed563@gmail.com>
2023-11-10 14:00:41 +0300

    Navbar component

--- cat/src/components/Navbar.jsx
+++ cat/src/components/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+     <div className="navbar-container">
+        <Link className="navbar-link" to={"/"}>
+          <h3 className="navbar-title">
+            <span className="material-symbols-outlined navbar-title-icon">
+              pets
+            </span>
+            <div className="navbar-title-text">Home</div>
+          </h3>
+        </Link>
+
+        <div className="navbar-right-part">
+          <div className="navbar-links">
+            <Link className="navbar-link" to={"/favorites"}>
+              <span className="material-symbols-outlined navbar-icons">
+                favorite
+              </span>
+              <div className="navbar-option">Favorites</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 export default Navbar;
